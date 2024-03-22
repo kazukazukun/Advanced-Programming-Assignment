b1b549f292b320c470a189515d9b766ae9d01bd0
Kazu-kun <kasundodanwala@gmail.com>
2024-03-22 11:40:10 +0530

    11:40 - 22/03/2024

--- Degrees of Hell/Degrees of Hell/Degrees of Hell.cpp
+++ Degrees of Hell/Degrees of Hell/Degrees of Hell.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
-{
+int main() {
+    // Open the text file
+    std::ifstream file("Resources/degrees.txt");
+
+    // Check if the file is open
+    if (!file.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    // Read and output the contents of the file
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    // Close the file
+    file.close();
+
     return 0;
 }
